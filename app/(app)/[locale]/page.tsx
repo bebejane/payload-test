046d872a6ec501c130f6fbf3f0a727eda104b29b
Bebe Jane <bjornthief@gmail.com>
2024-12-07 19:52:43 +0100

    h

--- app/(app)/[locale]/page.tsx
+++ app/(app)/[locale]/page.tsx
@@ -1,21 +1,20 @@
 import s from './page.module.scss'
-import cn from 'classnames';
-import { setRequestLocale } from 'next-intl/server';
-import Link from 'next/link';
-import { getPayload } from 'payload';
+import cn from 'classnames'
+import { setRequestLocale } from 'next-intl/server'
+import Link from 'next/link'
+import { getPayload } from 'payload'
 import configPromise from '@payload-config'
-import RichText from '@/lib/rich-text';
-import { defaultLocale } from '@/i18n';
-import { draftMode } from 'next/headers';
+import RichText from '@/lib/rich-text'
+import { defaultLocale } from '@/i18n'
+import { draftMode } from 'next/headers'
 import Image from 'next/image'
 
 export default async function Home({ params }: LocaleParams) {
-
   const { locale = defaultLocale } = await params
-  setRequestLocale(locale);
+  setRequestLocale(locale)
 
   const payload = await getPayload({ config: configPromise })
-  const draft = ((await draftMode()).isEnabled)
+  const draft = (await draftMode()).isEnabled
   const home = await payload.findGlobal({ slug: 'home', locale, draft })
   const data = await payload.find({ collection: 'posts', locale, draft })
   const posts = data.docs
@@ -23,11 +22,7 @@ export default async function Home({ params }: LocaleParams) {
   return (
     <article className={cn(s.start)}>
       <h1>{home.header}</h1>
-      <RichText data={home.content} />
-      {posts.map(post =>
-        <Link key={post.id} href={`/${locale}/posts/${post.slug}`}>{post.title}</Link>
-      )}
-      {typeof home.image === 'object' && home.image?.url &&
+      {typeof home.image === 'object' && home.image?.url && (
         <Image
           className={s.image}
           src={home.image.url}
@@ -35,8 +30,17 @@ export default async function Home({ params }: LocaleParams) {
           height={home.image.height ?? 0}
           alt={home.image.alt}
         />
-      }
+      )}
+      <RichText data={home.content} />
+      <h2>Latest posts</h2>
+      {posts.map((post) => (
+        <>
+          <Link key={post.id} href={`/${locale}/posts/${post.slug}`}>
+            {post.title}
+          </Link>
+          <br />
+        </>
+      ))}
     </article>
-
   )
 }
