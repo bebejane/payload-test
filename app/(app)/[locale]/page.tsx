41757a041beeed31d6424c77ecb04aa3b6d2c9ea
Bebe Jane <bjornthief@gmail.com>
2024-12-12 10:49:54 +0100

    ahhh

--- app/(app)/[locale]/page.tsx
+++ app/(app)/[locale]/page.tsx
@@ -22,7 +22,7 @@ export default async function Home({ params }: LocaleParams) {
 
   setRequestLocale(locale)
 
-  const draft = false //(await draftMode()).isEnabled
+  const draft = (await draftMode()).isEnabled
 
   const { Home } = await apiQuery<HomeQuery, HomeQueryVariables>(HomeDocument, {
     variables: { draft, locale: locale as LocaleInputType },
